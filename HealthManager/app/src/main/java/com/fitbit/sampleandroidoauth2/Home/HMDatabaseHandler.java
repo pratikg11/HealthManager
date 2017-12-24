bb2db0c891a7967daf81d056e4ac91efed1aa1dc
Pratik Ghogale <pratik.ghogale@gmail.com>
2017-12-24 05:59:20 -0600

    Update HMDatabaseHandler.java

--- HealthManager/app/src/main/java/com/fitbit/sampleandroidoauth2/Home/HMDatabaseHandler.java
+++ HealthManager/app/src/main/java/com/fitbit/sampleandroidoauth2/Home/HMDatabaseHandler.java
@@ -7,9 +7,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
-/**
- * Created by pratik on 7/30/2017.
- */
+
 
 public class HMDatabaseHandler extends SQLiteOpenHelper {
 
